4f081461f48db5b30ccd1103a0221f1453d6b433
p15poly <p15poly@ionio.gr>
2018-03-07 19:43:29 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -21,7 +21,7 @@ var entityCentres = {
 		individual: {x: w / 3.65, y: h / 3.3},
 	};
 
-var fill = d3.scale.ordinal().range(["#F02233", "#087FBD", "#FDBB30"]);
+var fill = d3.scale.ordinal().range(["#58C0B6", "#FE9814", "#FF57D2"]);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
